725c29547289e63c26c704c6a3e83e773b3e8d85
Jika1995 <jikulina2016@gmail.com>
2024-03-25 13:43:20 +0600

    Task-3 ready

--- task-3.js
+++ task-3.js
@@ -2,33 +2,14 @@ const fs = require('fs');
 const csv = require('csvtojson');
 const path = require('path');
 
-const csvPath = 'csv/nodejs-hw1-ex1.csv';
+const fileName = 'nodejs-hw1-ex1.csv';
+const csvPath = path.join('csv', fileName);
+const txtFileName = fileName.slice(0, -3) + 'txt';
 
-// const readCSVFile = fs.createReadStream(csvPath);
-
-// const writeCSVFile = request.put('csv/newFile.csv');
-
-// readCSVFile.pipe(csv()).pipe(writeCSVFile);
-
-async function csvParser(fileName) {
-  let jsonFileName = fileName.slice(0, -3) + 'json';
-  let filePath = path.join('csv', 'nodejs-hw1-ex1.csv');
-  const rawData = fs.readFile(filePath, { encoding: 'utf-8' }, (err, data) => {
-    if (err)
-      return console.error(err)
+const writeCSVFile = fs.createWriteStream(txtFileName);
+csv({ delimiter: ';' })
+  .fromFile(csvPath)
+  .subscribe((book) => {
+    const newBook = JSON.stringify(book)
+    writeCSVFile.write(`${newBook}\n`)
   });
-
-  const jsonArray = await csv().fromFile(filePath);
-
-  let res = JSON.stringify(jsonArray, null, 2);
-  console.log(res);
-
-  fs.writeFile(jsonFileName, res, (err) => {
-    if (err)
-      return console.error(err)
-
-    console.log('success');
-  })
-}
-
-csvParser('nodejs-hw1-ex1.csv')
